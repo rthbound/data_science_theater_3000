3fd6fe776ad637fa29d2b135d4c45eabfb71d03d
Tad Hosford <tad@isotope11.com>
2012-01-22 14:07:35 -0600

    Added text2people feature, Added ability to locate multiple ip addresses

--- lib/data_science_theater_3000.rb
+++ lib/data_science_theater_3000.rb
@@ -6,10 +6,14 @@ require "data_science_theater_3000/version"
 module DataScienceTheater3000
   # Converts an ip address into a location hash
   #
-  # @param [String] ip the ip address to be located
-  # @return [Hash] hash of location information. Can be passed as input to coordinates2politics
+  # @param [Array, String] ip Either an array of ip strings or a single ip address to be located
+  # @return [Hash] hash of location information. Singular output can be passed as input to coordinates2politics
   def self.ip2coordinates ip
     url = "http://www.datasciencetoolkit.org"
+    if ip.class == Array
+      ip.each {|i| i.gsub!(",", "%2c") }
+      ip = ActiveSupport::JSON.encode(ip)
+    end
     response = Curl::Easy.perform( url + "/ip2coordinates/" + ip ).body_str
 
     coordinates = make_hashy(response)
@@ -23,42 +27,49 @@ module DataScienceTheater3000
   # Converts a street address into a location hash
   #
   # @param [String, Array] address as either a single string or array of strings
-  # @return [Hash] hash of location information. Can be passed as input to coordinates2politics
+  # @return [Hash] hash of location information. Singular output can be passed as input to coordinates2politics
   def self.street2coordinates address
     url = "http://www.datasciencetoolkit.org"
     if address.class == String
       address.gsub!(",", "%2c").gsub!(" ", "+")
-      response = Curl::Easy.perform( url + "/street2coordinates/" + address ).body_str
     elsif address.class == Array
       address.each {|a| a.gsub!(",", "%2c").gsub!(" ", "+") }
       address = ActiveSupport::JSON.encode(address)
-      response = Curl::Easy.perform( url + "/street2coordinates/" + address ).body_str
     end
+    response = Curl::Easy.perform( url + "/street2coordinates/" + address ).body_str
+
     coordinates = make_hashy(response)
   end
   
   # Uses latitude,longitude pair to find detailed political information about a location.
   # Currently supporting a single pair.
   #
-  # @param [Hash,String] hash returned from ip2coordinates/street2coordinates or just a string like "33,-86"
+  # @param [Hash,String] coords hash returned from ip2coordinates/street2coordinates or just a string like "33,-86"
   # @return [Array] contains hashes with detailed political information for a location
   def self.coordinates2politics coords 
-    if coords.class == Hash
-      coords = coords.keys.map{|k| "#{coords[k]["latitude"]},#{coords[k]["longitude"]}"}.first
-    end
-
     url = "http://www.datasciencetoolkit.org"
+    if coords.class == Hash && coords.keys.count == 1
+      coords = coords.keys.map{ |k| "#{coords[k]["latitude"]},#{coords[k]["longitude"]}" }.first
+    end
     coords.gsub!( "," , "%2c" )
     response = Curl::Easy.perform( url + "/coordinates2politics/" + coords ).body_str
 
-    coordinates = make_hashy(response)
+    politics = make_hashy(response)
   end
   
+  # Specify a name or names and return parsed result as well as a best-guess for gender
+  #
+  # @param [Array, String] name can be array of name strings or a single name
+  # @return [Hash] information related to the provided name
   def self.text2people name
-    #url = "http://www.datasciencetoolkit.org"
-    #response = Curl::Easy.perform( url + "/text2people/" + name ).body_str
+    url = "http://www.datasciencetoolkit.org"
+    name = ActiveSupport::JSON.encode(name)
+    c = Curl::Easy.new( url + '/text2people')
+    c.multipart_form_post = true
+    c.http_post(Curl::PostField.content('body', name))
+    response = c.body_str
 
-    #person = make_hashy(response)
+    person = make_hashy(response)
   end
 
   def self.text2times text
